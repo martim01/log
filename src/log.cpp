097b105d58cac2c6dab01bc2124c78c2f88d8630
Matthew Martin <martim01@outlook.com>
2020-04-29 11:16:06 +0100

    First Commit

---
+++ src/log.cpp
@@ -0,0 +1,21 @@
+#include "log.h"
+#include <iostream>
+
+using namespace pml;
+
+const std::string Log::STR_LEVEL[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+Log& Log::Get(enumLevel eLevel)
+{
+    static Log lg;
+    lg.SetLevel(eLevel);
+    return lg;
+}
+
+
+void LogOutput::Flush(int nLogLevel, const std::stringstream&  logStream)
+{
+    //if(nLogLevel != Log::LOG_DEBUG)
+    std::cout << Log::STR_LEVEL[nLogLevel] << "\t" << logStream.str();
+}
+
