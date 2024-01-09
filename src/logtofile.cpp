9094384561fb1ffd0e83865cec399fdb866003bc
Matthew Martin <martim01@outlook>
2024-01-09 10:05:33 +0000

    fix: prefix

--- src/logtofile.cpp
+++ src/logtofile.cpp
@@ -183,7 +183,7 @@ void LogToFile::OpenFile(const std::string& sFilePath, const std::string& sFileN
     //chmod(sFile.c_str(), 0664);
 }
 
-void LogToFile::Flush(pml::enumLevel eLogLevel, const std::stringstream&  logStream)
+void LogToFile::Flush(pml::enumLevel eLogLevel, const std::stringstream&  logStream, const std::string& sPrefix)
 {
     if(eLogLevel >= m_eLevel)
     {
@@ -202,7 +202,7 @@ void LogToFile::Flush(pml::enumLevel eLogLevel, const std::stringstream&  logStr
         if(m_ofLog.is_open())
         {
             m_ofLog << Timestamp().str();
-            m_ofLog << pml::LogStream::STR_LEVEL[eLogLevel] << "\t" << logStream.str();
+            m_ofLog << pml::LogStream::STR_LEVEL[eLogLevel] << "\t" << "[" << sPrefix << "]\t" << logStream.str();
             m_ofLog.flush();
         }
     }
